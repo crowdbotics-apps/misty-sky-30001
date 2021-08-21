ec6e4337d542b2ea94551c313de787292294206f
Crowdbotics <team@crowdbotics.com>
2021-08-21 07:54:07 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "misty_sky_30001Identifier",
+  url: "https://misty_sky_30001Identifier.botics.co",
+  api: "https://misty_sky_30001Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
